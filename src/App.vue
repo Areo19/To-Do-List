88f000cc2efb8901dfdd208e22dc665db5964fc9
Dominik Gniadek <d.gniadek@digitalcolliers.com>
2023-09-04 20:15:06 +0200

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,87 @@
 <template>
-  <div class="to_do_container">
+  <div class="task-container">
     <h1>To Do List</h1>
-    <div class="adding-new-position">
-      <input v-model="inputText" class="input-box" placeholder="Add new position..."/>
-      <button @click="addElement" class="btn-add">Add</button>
+    <div class="add-new-position">
+      <input 
+        v-model="inputText" 
+        class="input-box" 
+        placeholder="Add new position..."
+      />
+      <button 
+        @click="addTask" 
+        class="btn-add"
+        >
+        Add
+      </button>
     </div>
-    <ul class="to-do-list">
-      <li v-for="(task, index) in tasks" :key="index">
-        <span :contenteditable="task.isEditing" class="to-do-text" tabindex="0">{{ task.text }}</span>
-        <button class="btn-edit" @click="editElement(task, index)">{{ task.isEditing ? 'Save' : 'Edit' }}</button>
-        <button class="btn-delete" @click="deleteElement(index)">Delete</button>
+    <ul class="task-list">
+      <li 
+        v-for="(task, index) in tasks" 
+        :key="index">
+          <span 
+          :contenteditable="task.isEdited" 
+          class="task-text" 
+          tabindex="0"
+          >
+          {{ task.text }}
+          </span>
+          <button 
+            class="btn-edit" 
+            @click="editTask(task, index)"
+            >
+            {{ task.isEdited ? 'Save' : 'Edit' }}
+          </button>
+          <button 
+            class="btn-delete" 
+            @click="deleteTask(index)"
+            >
+            Delete
+          </button>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
 
+export default {
+  name: 'ToDoList',
+  setup() {
+    const inputText = ref('');
+    const tasks = ref([]);
+
+   function addTask(){
+    if(inputText.value){
+      tasks.value.push({ text: inputText.value });
+      inputText.value = '';
+    }
+   }
+
+   function deleteTask(index){
+    tasks.value.splice(index, 1)
+   }
+
+   function editTask(task, index){
+    if(task.isedited){
+      task.text = task.text;
+      tasks.value.splice(task, index, 1)
+    }
+    task.isEdited =  !task.isEdited
+   }
+
+   return{
+    inputText,
+    tasks,
+    addTask,
+    deleteTask,
+    editTask
+   }
+  }
+};
+</script>
+
+<!-- <script>
 export default {
   data() {
     return {
@@ -33,18 +98,18 @@ export default {
     }
   },
   methods: {
-    addElement() {
+    addTask() {
       if (this.inputText) {
-        this.tasks.push({ text: this.inputText, isEditing: false });
+        this.tasks.push({ text: this.inputText, isEdited: false });
         this.inputText = "";
       }
     },
-    deleteElement(index) {
+    deleteTask(index) {
       this.tasks.splice(index, 1);
     },
-    editElement(task) {
-      task.isEditing = !task.isEditing;
+    editTask(task) {
+      task.isEdited = !task.isEdited;
     },
   }
 };
-</script>
+</script> -->
